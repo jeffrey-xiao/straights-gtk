ae23f676c564376915a75a7e46107471c6f6c632
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-07-06 14:08:59 -0400

    Add spec to GameController.h and Game.h

--- Game.h
+++ Game.h
@@ -12,6 +12,16 @@ struct Command;
 class Observer;
 
 class Game {
+  // Represents the state of a game of straights
+  // Specification Fields:
+  //   ROUND_START: Represents that the game of straights has started
+  //   HUMAN_INPUT: Represents that the game is waiting on a human player
+  //   INVALID_PLAY_INPUT: Represents that the last human play input is invalid
+  //   INVALID_DISCARD_INPUT: Represents that the last human discard input is invalid
+  //   DISCARDED_CARD: Represents that a card has been discarded in the game
+  //   PLAYED_CARD: Represents that a card has been played in the game
+  //   ROUND_END: Represents that a round has ended
+  //   GAME_END: Represents that the game has ended
   public:
     enum class GameState {
       ROUND_START,
@@ -24,21 +34,56 @@ class Game {
       GAME_END,
     };
     Game(int, std::vector<PlayerType>, Observer*);
+    // ensures: initializes this to a new Game with a specified seed and list of Players
 
     std::vector<Card> getCurrentPlayerCards() const;
+    // returns: a list of cards associated with the current player in the game
+
     std::vector<Card> getCurrentPlayerValidCards() const;
+    // returns: a list of cards associated with the current player that can be played on the boar
 
     void startRound();
+    // modifies: this
+    // ensures: the deck is reshuffled, all hands are dealt to players, the discard piles are
+    //          emptied, and a new round of straights is started
+
     void playCard(Card);
+    // modifies: this
+    // ensures: if the card is able to be played, then the game state is changed to PLAYED_CARD and
+    //          the card is removed from the hand of the player and put onto the game board. If the
+    //          card is an invalid play, then the game state is changed to INVALID_PLAY_INPUT. The
+    //          state of the game board and hand does not change.
+
     void discardCard(Card);
+    // modifies: this
+    // ensures: if the card is able to be discarded, then the game state is changed to
+    //          DISCARDED_CARD and the card is removed from the hand of the player and put in the
+    //          player's discard pile. If the card is an invalid discard, then the game state is
+    //          changed to INVALID_DISCARD_INPUT. The state of the game board, hand, and discard
+    //          pile does not change
+
     void ragequit();
+    // modifies: this
+    // ensures: the current player changed to a computer player
 
     GameBoard getGameBoard() const;
+    // returns: the game board associated with the game
+
     std::vector<Player> getPlayers() const;
+    // returns: a list of players that are participating in the game
+
     int getCurrentPlayer() const;
+    // returns: the id of the current player in the game
+
     Card getLastCard() const;
+    // returns: the last played or discarded in the game. In the case that there is no last card
+    //          played, the ace of spaces will be returned.
+
     Deck getDeck() const;
+    // returns: the deck of cards in the game
+
     GameState getGameState() const;
+    // returns: the state of the game
 
   private:
     int seed_;
@@ -51,8 +96,18 @@ class Game {
     Card lastCard_;
 
     void notify();
+    // modifies: this
+    // ensures: the user interface is updated
+
     void runRound();
-    void setGameState(GameState);
+    // modifies: this
+    // ensures: plays the first valid card or discards the first hand if there are no valid cards
+    //          until the current player is a human player
+
+    void setGameState(GameState gameState);
+    // modifies: this
+    // ensures: gameState_ is set to gameState and the user interface is notified of the change in
+    //          state
 };
 
 #endif
--- GameController.h
+++ GameController.h
@@ -12,25 +12,55 @@ class Player;
 enum class PlayerType;
 
 class GameController {
+  // Handles communication between the model (Game) and the view (TextUserInterface)
+  // Specification Fields:
+  //   game = representation of the state of a game of straights
   private:
     Game *game_;
 
   public:
-    GameController(int, std::vector<PlayerType>, Observer *);
+    GameController(int seed, std::vector<PlayerType>, Observer *);
+    // ensures: initializes this to a new GameController with a new Game
+
     ~GameController();
+    // modifies: this
+    // ensures:  this no longer exists; memory is deallocated
 
     void startGame();
-    void executeCommand(Command);
+    // modifies: this
+    // ensures: the game is started
+
+    void executeCommand(Command command);
+    // modifies: this
+    // ensures: the command is executed in the game
 
     GameBoard getGameBoard() const;
+    // returns: the game board associated with the game
+
     std::vector<Player> getPlayers() const;
+    // returns: a list of players that are participating in the game
+
     std::vector<int> getWinners() const;
+    // returns: a list of player id that have the lowest scores in the game
+
     int getCurrentPlayer() const;
+    // returns: the id of the current player in the game
+
     Card getLastCard() const;
+    // returns: the last card played or discarded in the game. In the case that there is no last
+    //          card played, the ace of spades will be returned.
+
     std::vector<Card> getCurrentPlayerCards() const;
+    // returns: a list of cards associated with the current player in the game
+
     std::vector<Card> getCurrentPlayerValidCards() const;
+    // returns: a list of cards associated with the current player that can be played on the board
+
     Deck getDeck() const;
+    // returns: the deck of cards in the game
+
     Game::GameState getGameState() const;
+    // returns: the state of the game
 };
 
 #endif
